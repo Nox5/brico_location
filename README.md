6f5073f73996210aa39c97dc803ebc5a08825df7
Jean-Philippe <jp.desmet1@gmail.com>
2021-05-17 15:27:03 +0200

    test

---
+++ README.md
@@ -0,0 +1,20 @@
+# brico_location
+bricoLocation.fr
+
+Site de mise en relation entre particuliers, pour la location de matériels de bricolage.
+
+Technologies utilisés:
+  Serveur : Langage : PHP => framework Symfony
+  
+  Client :
+    Langages : HTML, CSS => framework Axentix
+               Sass
+               JavaScript
+               
+ Fonctionnalités :
+    Fonctionnalités générales:
+      - Système d'inscription avec validation du compte.
+      - Système de connexion
+      - Système de recherche
+      - Système de gestion de profil
+      
---
+++ src/Entity/Category.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
